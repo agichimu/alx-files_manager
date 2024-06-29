49e9c7e799a31cb61eb03fe88c7c12b4a0dcf911
agichimu <alexandergichimu@zetech.ac.ke>
2024-06-29 18:36:25 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -23,6 +22,28 @@ class UsersController {
     const result = await dbClient.db.collection('users').insertOne(newUser);
 
     return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
