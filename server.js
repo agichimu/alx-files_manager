1720c4417db38d1bdc51dab3bf2c5fd5544c346b
agichimu <alexandergichimu@zetech.ac.ke>
2024-06-29 18:25:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require('express');
+import express from 'express';
+import routes from './routes/index';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-const routes = require('./routes/index');
-
+app.use(express.json());
 app.use('/', routes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
