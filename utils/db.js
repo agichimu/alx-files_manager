63017ec1a22a28802de17184c5e8b725588bd12f
agichimu <alexandergichimu@zetech.ac.ke>
2024-07-15 16:15:32 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,20 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-
     this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(database);
-      })
-      .catch((err) => console.log('MongoDB Client Error', err));
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error('Failed to connect to MongoDB', err);
+    });
   }
 
   isAlive() {
-    return !!this.db;
+    return this.client.isConnected();
   }
 
   async nbUsers() {
@@ -25,7 +24,20 @@ class DBClient {
   async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
+
+  async getUserByEmail(email) {
+    return this.db.collection('users').findOne({ email });
+  }
+
+  async getFileById(id) {
+    return this.db.collection('files').findOne({ _id: ObjectId(id) });
+  }
+
+  async createFile(fileDocument) {
+    const result = await this.db.collection('files').insertOne(fileDocument);
+    return { id: result.insertedId, ...fileDocument };
+  }
 }
 
-const dbClient = new DBClient();
+export const dbClient = new DBClient();
 export default dbClient;
